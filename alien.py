cb55f58153fd8cbd9e8378b72135997ac08e903c
Konstantin White <konstantin.belopuhov@gmail.com>
2021-02-07 17:03:35 +0800

    Adding new alien skins.

--- alien.py
+++ alien.py
@@ -6,9 +6,10 @@ class Alien():
         """Initializes the alien and sets its starting position."""
         self.screen = screen
 
-        # Можно сделать цыкл который будет создавать пришельцев с разными скинами.
+    # Можно сделать цыкл который будет создавать пришельцев
+    # с разными скинами.
         # Loading a alien image and getting a rectangle.
-        self.image = pygame.image.load('images/aliens/alien.png')
+        self.image = pygame.image.load('images/aliens/alien1.png')
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
         # Each new alien appears at the center of the screen.
