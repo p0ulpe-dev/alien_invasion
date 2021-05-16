615a0fcc108f04bc133bde21e5126ec54bd9745e
Konstantin White <konstantin.belopuhov@gmail.com>
2021-05-16 17:43:56 +0800

    Resetting the game.

--- game_functions.py
+++ game_functions.py
@@ -35,7 +35,8 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats,
+                 play_button, ship, aliens, bullets):
     """Handles keys and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -46,13 +47,29 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_keyup_events(event, ship)
         elif event.type == MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen,
+                              stats, play_button,
+                              ship, aliens, bullets,
+                              mouse_x, mouse_y)
 
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen,
+                      stats, play_button,
+                      ship, aliens, bullets,
+                      mouse_x, mouse_y):
     """Starts a new game when you click Play."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset stats
+        stats.reset_stats()
         stats.game_active = True
+
+        # Clean groups of aliens and bullets
+        aliens.empty()
+        bullets.empty()
+
+        # Creating a new fleet and placement of the ship to the center.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 
 def update_screen(ai_settings, screen, stats, ship,
